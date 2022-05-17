1f201183fc8d251c6a9df8e77a1d6d7336a0e3cc
Kevin Byrom <kevinbyrom@gmail.com>
2022-05-17 16:34:58 -0700

    Initial Commit

---
+++ SampleIOSApp/SampleIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleIOSAppApp.swift
+//  SampleIOSApp
+//
+//  Created by Kevin on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
